feddfc8f69a72901c80a79a6274b578ec0401361
Feng(Steve) yuan <yuanf02@gmail.com>
2025-07-15 12:16:58 -0700

    update new practice

---
+++ forloop_practice.py
@@ -0,0 +1,19 @@
+# user_name
+
+name_list = ["steve", "grace", "hongbian"]
+
+# password = "py1234"
+
+# name_input = input("Please enter your name: ").strip().lower()
+
+# while True:
+
+#     # password_input = input("please enter your password: ").strip().lower()
+#     for name in name_list:
+#         if name == name_input:
+#             print(f"welcome home!!!! {name_input}")
+#         else:
+#             print(f"{name_input} you are not my familly!!!!!!")
+#     break
+
+print(name_list[3])
