b22ddbc53d76359261d4667f4b28914b471a9ad4
Feng(Steve) yuan <yuanf02@gmail.com>
2025-08-12 06:44:32 -0700

    Day1_Practice

---
+++ Python_With_Grace/week_time.py
@@ -0,0 +1,12 @@
+# Calculate how many minutes are in a week?
+
+# TODO: How many days are in a week?
+days = 7
+# TODO: How many hours are in a day?
+hours = 24
+# TODO: How many minutes are in a hour?
+minutes = 60
+# TODO: Formula: calculation
+total_minutes = minutes * hours * days
+# TODO: Print the results.
+print(f"There are {total_minutes} minutes in a week.")
---
+++ Python_With_Grace/your_name.py
@@ -0,0 +1,19 @@
+# your name
+
+# TODO: input your name?
+name = input("What is your name? ")
+# TODO: input your birthday?
+bday = input("When is your birthday? ")
+# TODO: input your gender?
+gender = input("are you a girl or a boy? ")
+# TODO: input your favorite color?
+color = input("What is your favorite color? ")
+# TODO: output 4 sentences:
+# Your name is XXX
+# Your birthday is mmm-dd-yyyy
+# Your gender is male/female.
+# Your favorite color is ###
+print(f"Your name is {name}.")
+print(f"Your birthday is on {bday}.")
+print(f"You are a {gender}.")
+print(f"Your favorite color is {color}.")
