301e10e4b88e1b300554105ed1ef6eec213b4f1d
Feng(Steve) yuan <yuanf02@gmail.com>
2025-09-10 19:49:04 -0700

    square.py

---
+++ Python_With_Grace/square.py
@@ -0,0 +1,26 @@
+# #TODO: what is the length and width of the perimiter?
+# edge = input ("What is the length and width of the perimiter?\nPlease answer here: ")
+# #TODO: convert edge and area from int to float.
+# edge = float(edge)
+# area = float(area)
+# #TODO: get the answer for the area.
+# area = edge * 4
+# #TODO: convert edge and area from int to float.
+# edge = float(edge)
+# area = (round(area),1)
+# #TODO: formula: cauculation.
+
+# #TODO: print the answer.
+# print
+
+# TODO: what is the length and width of the perimiter
+edge = input("What is the length and width of the perimiter?\nAnswer here\n: ")
+# TODO: convert from string to float
+edge = float(edge)
+# TODO: cauculate the area: area = edge * edge
+area = round(edge * edge, 1)
+# TODO: edge all = edge * 4
+edge_all = round(edge * 4, 1)
+# TODO: print the answer
+print(
+    f"The perimiter of the room would be {edge_all} and the area of the room would be {area}.")
